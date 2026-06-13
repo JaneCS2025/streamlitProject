18a71f0450fa1e16195fe5c58034accd0572464f
JaneCS2025 <arialiu11220@gmail.com>
2026-06-12 18:48:56 -0700

    add text and button to main.py file

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Welcome to me web page! 🥳🥳🥳")
+st.header("This is the header")
+st.subheader("This is the subheader")
+
+st.divider()
+
+st.markdown(
+  '''
+     This is the playground for you to try streamlit and have fun.
+     **There's :rainbow[so much] you can build!**
+    
+      We prepared a few examples for you to get started. Just 
+      click on the buttons above and discover what you can do   
+      with Streamlit. 
+
+  '''
+)
+
+if st.button("Send balloons!"):
+  st.balloons()
+
+st.button("click me")
