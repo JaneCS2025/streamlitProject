c19fd08879ef88268c930e7c1c7c91cc9a45cbda
JaneCS2025 <arialiu11220@gmail.com>
2026-06-20 23:22:47 -0700

    Modify slot machine code

--- slotmachine.py
+++ slotmachine.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import random
 import time
+from streamlit_extras.let_it_rain import rain
 
 ##https://janeslotmachine.streamlit.app/
 
 symbols = ['🍒', '🍿', '🎁', '🐸', '⭐️']
 
-st.title("🎰 WIN BIG SLOT MACHINE 🥳🥳🥳")
-st.write("Press the button to spin")
-
-st.divider()
+st.image('https://prodimages.everythingneon.com/giant/l100-7798-casino-animated-led-sign.gif')
 
 #session state - python object store value in memory
 # st.write('st.session_state', st.session_state)
@@ -28,34 +26,123 @@ if st.button('🎲 SPIN Match 2 for some coins, match 3 for the jackpot!'):
     box3 = col3.empty()
 
     for i in range(15):
-        box1.header(random.choice(symbols))
-        box2.header(random.choice(symbols))
-        box3.header(random.choice(symbols))
+        box1.markdown(
+            f"<div style='font-size:120px;text-align:center'>{random.choice(symbols)}</div>",
+            unsafe_allow_html=True
+        )
+
+        box2.markdown(
+            f"<div style='font-size:120px;text-align:center'>{random.choice(symbols)}</div>",
+            unsafe_allow_html=True
+        )
+        box3.markdown(
+            f"<div style='font-size:120px;text-align:center'>{random.choice(symbols)}</div>",
+            unsafe_allow_html=True
+        )
         time.sleep(0.2)
 
     s1 = random.choice(symbols)
     s2 = random.choice(symbols)
     s3 = random.choice(symbols)
 
-    box1.header(s1)
-    box2.header(s2)
-    box3.header(s3)
+    box1.markdown(
+            f"<div style='font-size:120px;text-align:center'>{s1}</div>",
+            unsafe_allow_html=True
+        )
+    box2.markdown(
+            f"<div style='font-size:120px;text-align:center'>{s2}</div>",
+            unsafe_allow_html=True
+        )
+    box3.markdown(
+            f"<div style='font-size:120px;text-align:center'>{s3}</div>",
+            unsafe_allow_html=True
+        )
 
 ###### Write the condition logic here #####
     if s1 == s2 == s3:
-        st.success("🎉🎉🎉 Jackpot! 3 match!! You won 100 points and 50 coins!!")
+        
         st.session_state.score +=100
         st.session_state.coin +=50
-        st.snow()
+        text = '🎉🍿⭐️Jackpot! 3 match!! You won 100 points and 50 coins!!'
+        st.markdown(
+            f"<div style='font-size:20px; color: white'>{text}</div>",
+            unsafe_allow_html=True
+        )
+        st.image('https://condaluna.com/assets/stickers/congrats.gif')
+       
+        
+        
     elif s1 == s2 or s1 == s3 or s2 == s3:
-        st.success("✨ Nice! 2 match! You won 10 coins!")
+        if s1 == s2:
+            matched_symbol = s1
+        elif s1 == s3:
+            matched_symbol = s1
+        else:  # s2 == s3
+            matched_symbol = s2
+        
         st.session_state.score +=10
         st.session_state.coin +=10
-        st.balloons()
-    else:
-        st.info("So close! Try again 😝")
+        rain(
+                emoji= matched_symbol,
+                font_size=100,
+                falling_speed=5,
+                animation_length=1
 
-st.write(f'### ⭐️ Your score: {st.session_state.score}')
+            )
+       
+    else:
+        rain(
+            emoji='💩',
+            font_size=100,
+            falling_speed=5,
+            animation_length=0.5
+        )
+       
+
+# st.write(f'### ⭐️ Your score: {st.session_state.score}')
 # st.write(f'### 🪙 Your coin: {st.session_state.coin}')
+
+st.markdown(
+  f"### ⭐️ <span style='color: white'>  Your score:  {st.session_state.score}</span>", unsafe_allow_html=True)
+st.markdown(
+  f"### 💰 <span style='color: white'>  Your coin:  {st.session_state.coin}</span>", unsafe_allow_html=True)
         
 
+##Add background image
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://static.vecteezy.com/system/resources/thumbnails/003/810/467/small/casino-advertising-neon-banner-design-with-playing-cards-and-casino-chips-on-purple-background-vector.jpg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("""
+<style>
+div.stButton > button {
+    background-color: purple;
+    color: white;
+    font-size: 50px;
+    font-weight: bold;
+    border-radius: 12px;
+    border: none;
+    width: 500px
+}
+
+/* Disable hover effect */
+div.stButton > button:hover {
+    background-color: purple !important;
+    color: white !important;
+    border: none !important;
+    transform: none !important;
+    box-shadow: none !important;
+}
+</style>
+""", unsafe_allow_html=True)
