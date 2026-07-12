e714c4bb6fcd7b70844e24ba71b97bb0757c6714
JaneCS2025 <arialiu11220@gmail.com>
2026-07-11 18:58:59 -0700

    Add sidebar

--- SidebarPage.py
+++ SidebarPage.py
@@ -1,20 +1,18 @@
 import streamlit as st
 
-st.logo("https://play-lh.googleusercontent.com/2SL3gubcMu57VsxUCBQX5unIzAkm2kdTB8aZwPoxu39u02dxIwxlYGwlfu8gimNWrE3HzzavYcggk89rwYZp0bg", size="large")
+st.logo("https://i.pinimg.com/736x/8c/6d/db/8c6ddb5fe6600fcc4b183cb2ee228eb7.jpg", size='large', link="https://www.youtube.com/watch?v=dQw4w9WgXcQ" )
 
 pages = {
-  "Daily Life":[
-    st.Page("page1.py", title="Page 1"),
-    st.Page("page2.py", title="Page 2")
-  ],
-  "Food": [
-    st.Page("food.py", title="Chicken Salad"),
-  ],
-  "Game": [
-    st.Page("game.py", title="Slot Machine")
-  ]
-}
+ "Daily Life": [
+   st.Page("page1.py", title="Self Introduction"),
+   st.Page("page2.py", title="Page 2")
+ ],
+ "Food": [
+   st.Page("food.py", title="Self Introduction"),
+ ]
 
+}
 
 pg = st.navigation(pages)
 pg.run()
+
