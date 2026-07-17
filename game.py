9afd22f5c32d011b0034e04e41d2e92c65f0d000
JaneCS2025 <arialiu11220@gmail.com>
2026-07-17 16:18:44 -0700

    Add audio

--- game.py
+++ game.py
@@ -23,7 +23,11 @@ st.markdown(
         background: transparent !important;
        }
 
-    </style>     
+       [data-testid="stAudio"] {
+        display: none;
+       }
+
+    </style>
 
    """,
    unsafe_allow_html=True
@@ -53,10 +57,21 @@ def showBanner(text):
       f'<p style="font-size: 25px; color: yellow">{text}</p>'
    ,  unsafe_allow_html=True)
 
+def showCornerImage(url, duration=5):
+   placeholder = st.empty()
+   placeholder.markdown(
+      f'<div style="position: fixed; bottom: 0; left: 0;"><img src={url} width=300></img></div>',
+       unsafe_allow_html=True
+   )
+   time.sleep(duration)
+   placeholder.empty()
+
 showUI('⭐️', ' Your score : ', st.session_state.score)
 showUI('💰', ' Your coin : ', st.session_state.coin)
 
 if st.button("Press to spin 🎲"):
+   spin_sound = st.empty()
+   spin_sound.audio("audio/spin.wav", autoplay=True)
    col1, col2, col3 = st.columns(3)
    box1 = col1.empty()
    box2 = col2.empty()
@@ -68,7 +83,8 @@ if st.button("Press to spin 🎲"):
       show_symbol(box1, random.choice(symbols))
       show_symbol(box2, random.choice(symbols))
       show_symbol(box3, random.choice(symbols))
-      time.sleep(0.2)
+      time.sleep(0.1)
+   spin_sound.empty()   # stops the spin sound when spinning ends
 
    s1 = random.choice(symbols)
    s2 = random.choice(symbols)
@@ -77,33 +93,41 @@ if st.button("Press to spin 🎲"):
    show_symbol(box1, s1)
    show_symbol(box2, s2)
    show_symbol(box3, s3)
+ 
 
-   if s1 == s2 == s3:
+   clapping_sound = st.empty()
+   if s1 == s2 == s3:  
+      st.audio("audio/coin.wav", autoplay=True)
+      clapping_sound.audio("audio/clapping.wav", autoplay=True)
       st.session_state.score +=10
       st.session_state.coin +=10
       rain(
          emoji=s1,
          font_size=100,
          falling_speed=5,
-         animation_length='10'
+         animation_length=1
       )
       showBanner("🎉🎉🎉 Wow, you win, 3 match! Jackpot!")
+      showCornerImage('https://i.pinimg.com/originals/86/96/59/869659344a7740d7692b439c01120c80.gif')
+      clapping_sound.empty()
    elif s1 == s2 or s1 == s3 or s2 == s3:
       if s1 == s2 or s1==s3:
          em = s1
       elif s2==s3:
          em = s2
-
+      st.audio("audio/coin.wav", autoplay=True)
       showBanner(" 2 match! Awesome 🤩!")
-      st.session_state.score +=5
-      st.session_state.coin +=5
-      # st.balloons()
+      clapping_sound.audio("audio/clapping.wav", autoplay=True)
       rain(
          emoji=em,
          font_size=100,
          falling_speed=5,
-         animation_length='10'
+         animation_length=1
       )
+      st.session_state.score +=5
+      st.session_state.coin +=5
+      showCornerImage('https://i.pinimg.com/originals/78/78/a2/7878a20aaed4de2b44a8b61fc38e9a36.gif')
+      clapping_sound.empty()
    else:
       showBanner("So close! Try again 😀")
 
