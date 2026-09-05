ae74f7866cc74dbf21a3c000ff10adf05fd10279
JaneCS2025 <arialiu11220@gmail.com>
2026-09-04 19:04:28 -0700

    Update regression

--- machine_learning.py
+++ machine_learning.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy
 from scipy import stats
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 st.header("Mean, Median and Mode")
 
@@ -188,18 +189,176 @@ st.pyplot(fig)
 
 st.code(notes, language='python')
 
+st.header("Regression")
+st.markdown("""
+The term regression is used when you try to find the relationship between variables
+""")
+
+st.subheader("Linear Regression")
+
 x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
 y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
+st.markdown("""
+r - the relationship - the coeffiecient of correlation - value ranges from -1 to 1
+    0 means no relationship, and 1 (and -1) means 100% relationship
+
+p - p < 0.05 -> significant evidence of linear relationship
+    p >= 0.05 -> not enough evidence to confirm a linear relationship 
+""")
+
+
+st.notes = """
+# Plot a linear regression model
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
 fig, ax = plt.subplots()
-ax.scatter(x, y)
 
-ax.set_xlabel("X Axis")
-ax.set_ylabel("Y Axis")
+ax.scatter(x, y) # scatter plot
+ax.plot(x, mymodel) # line chart
 
-ax.set_title("Scatter Plot")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_title("Scatter Plot with Linear Regression")
+
 st.pyplot(fig)
 
+st.write("Slope", slope)
+st.write("Intercept", intercept)
+st.write("r", r)
+st.write('p value', p)
+
+speed = myfunc(10)
+st.write('Prediction: 10 year old car the speed is', speed)
+
+"""
+st.code(notes, language='python')
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
+fig, ax = plt.subplots()
+
+ax.scatter(x, y) # scatter plot
+ax.plot(x, mymodel) # line chart
+
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_title("Scatter Plot with Linear Regression")
+
+st.pyplot(fig)
+
+st.write("Slope", slope)
+st.write("Intercept", intercept)
+st.write("r", r)
+st.write('p value', p)
+
+speed = myfunc(10)
+st.write('Prediction: 10 year old car the speed is', speed)
+
+notes = """
+# Bad example - non linear regression model
+x1 = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
+y1 = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15]
+
+slope, intercept, r, p, std_err = stats.linregress(x1, y1)
+
+mymodel1 = list(map(myfunc, x1))
+
+
+fig, ax = plt.subplots()
+
+ax.scatter(x1, y1) # scatter plot
+ax.plot(x1, mymodel1) # line chart
+
+ax.set_xlabel("X1")
+ax.set_ylabel("Y1")
+ax.set_title("Scatter Plot with Linear Regression")
+st.pyplot(fig)
+
+st.write("Slope", slope)
+st.write("Intercept", intercept)
+st.write("r", r)
+st.write('p value', p)
+"""
+st.code(notes, language='python')
+
+x1 = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
+y1 = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15]
+
+slope, intercept, r, p, std_err = stats.linregress(x1, y1)
+
+mymodel1 = list(map(myfunc, x1))
+
+
+fig, ax = plt.subplots()
+
+ax.scatter(x1, y1) # scatter plot
+ax.plot(x1, mymodel1) # line chart
+
+ax.set_xlabel("X1")
+ax.set_ylabel("Y1")
+ax.set_title("Scatter Plot with Linear Regression")
+st.pyplot(fig)
+
+st.write("Slope", slope)
+st.write("Intercept", intercept)
+st.write("r", r)
+st.write('p value', p)
+
+st.subheader("Polynomial Regression")
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+# Ploynomial Regression, degress = 5
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 5))
+
+myline = numpy.linspace(1, 22, 100)
+
+fig, ax = plt.subplots()
+ax.scatter(x,y)
+ax.plot(myline, mymodel(myline))
+
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_title("Scatter Plot with Polynomial Regression - Degree 5")
+
+st.pyplot(fig)
+
+st.markdown("""
+r-squared value - value ranges from 0 to 1, where 0 means no relationship, and 1 means 100% related
+""")
+
+st.write("R2 score", r2_score(y, mymodel(x))) # 0.95
+
+speed = mymodel(17)
+
+st.write("Predict the speed of 17 year old car", speed) # 79 - 90
+
+st.subheader("Another example")
+
+# Bad data for Ploynomial Regression
+
+x = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
+y = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15]
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+myline = numpy.linspace(2, 95, 100)
+
+fig, ax = plt.subplots()
+ax.scatter(x, y)
+ax.plot(myline, mymodel(myline))
+st.pyplot(fig)
+
+st.write("R2 score",  r2_score(y, mymodel(x))) 
 
 
 
