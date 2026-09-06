0ee29e27de44df51ca84a21d1f18407a899c51d7
JaneCS2025 <arialiu11220@gmail.com>
2026-09-05 19:08:23 -0700

    add list comprehension

--- list.py
+++ list.py
@@ -20,47 +20,50 @@ Why Learn DSA with Python: \n
 st.subheader("List")
 
 notes = """
-# Square each number in the list
-a=[2,3,4,5]
-#way1
-output = list(map(lambda x: x ** 2, a))
-#way2
-res = [val ** 2 for val in a]
-#output
-# [4, 9, 16, 25]
+# create a list by squaring each number in a
+a = [2, 3, 4, 5]
 
-# New list return only even numbers
+#[4, 9, 16, 25]
+res = [ val ** 2 for val in a]
+# print('res', res) # [4, 9, 16, 25]
+
+# create a new list with only even number in a
 a = [1, 2, 3, 4, 5]
-res = [val for val in a if val % 2 == 0]
-#output
-# [2, 4]
+res = [val for val in a if val %2 ==0 ]  # [2, 4]
+# print(res)
 
-# New list return result based on condition
-a = [5, 12, 7, 18, 3, 20] # [3, 5, 7, 12, 18, 20]
-res = [val for val in a if val >10]
-res = [a[i] for i in range(len(a)) if i % 2 ==1]
-#output:
-# [12, 18, 20]
-print(res)
+b = [5, 12, 7, 18, 3, 20] # [3, 5, 7, 12, 18, 20]
+#[12, 18, 20]
 
-# Create a list from a range
-#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-res = [i for i in range(10)]  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print([1]*5) # [1, 1, 1, 1, 1]
+res = [val for val in b if val >= 12 ]
+print(res) # [12, 18, 20]
 
-# Flatten the list 
-a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # [ val1, val2, val3] -> val1 = [1, 2, 3]
-#output 
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+res = [i for i in range(10)]
+print(res) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(list(range(10)))
+
+print([1] * 5) #[1, 1, 1, 1, 1]
+
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
 #[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-res = [val for item in a for val in item]
+# res = []
+# for x in mat:
+#   for val in x:
+#     res.append(val)
+
+# print(res) #[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+res = [x for row in mat for x in row] # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 print(res)
 
 """
 
 st.code(notes, language='python')
-
-
 
 
 
